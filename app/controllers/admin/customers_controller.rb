7e2f8d96299fdbe2c7ade1d0e0bf37c0b04ac99a
EC2 Default User <ec2-user@ip-172-31-2-114.ec2.internal>
2022-04-16 09:04:10 +0000

    Add_会員一覧画面作成・詳細ページ仮作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,8 +1,11 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers=Customer.page(params[:page])
   end
 
   def show
+    @customer=Customer.find(params[:id])
+    @customers=Customer.page(params[:page])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,21 @@
 Rails.application.routes.draw do
 
-
-
-  devise_for :public, controllers: {
-        sessions: 'public/sessions',
-        registrations: 'public/registrations'
-      }
+  devise_for :customers, controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations' # signup view -> app/controllers/public/registrations_controller.rb
+  }
   
   devise_for :admin, controllers: {
-        sessions: 'admin/sessions',
-        registrations: 'admin/registrations'
-      }
+    sessions: 'admin/sessions',
+    registrations: 'admin/registrations'
+  }
 
   get '/admin' => 'admin/homes#top'
-    namespace :admin do
-      resources :genres,only:[:index,:create,:edit,:update]
-      resources :items,only:[:index,:new,:create,:show,:edit,:update] ,param: :id
-      resources :customers,only:[:index,:show,:edit,:update]
-    end
-
+  namespace :admin do
+    resources :genres,only:[:index,:create,:edit,:update]
+    resources :items,only:[:index,:new,:create,:show,:edit,:update] ,param: :id
+    resources :customers,only:[:index,:show,:edit,:update]
+  end
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
