76531cc58ff0e236a40bb19466f0fe9301e0c4dd
EC2 Default User <ec2-user@ip-172-31-2-114.ec2.internal>
2022-05-02 15:26:44 +0000

    Add_レイアウト仮修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,12 +4,12 @@ class Item < ApplicationRecord
     has_many :order_details, dependent: :destroy
     has_many :cart_items, dependent: :destroy
     
-    def get_item_image
-    unless item_image.attached?
-      file_path = Rails.root.join('app/assets/images/NoImage.jpg')
-      item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    item_image.variant(resize_to_limit: [100, 100]).processed
+    def get_item_image(width, height)
+      unless item_image.attached?
+        file_path = Rails.root.join('app/assets/images/NoImage.jpg')
+        item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      end
+      item_image.variant(resize_to_limit: [width, height]).processed
     end
     
     def add_tax_price
